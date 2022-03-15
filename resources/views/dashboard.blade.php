cd30a6fe5b03a02f28bcdb0d930f03c5e9c19043
6210743073 <poohmiphan@outlook.com>
2022-03-15 19:37:59 +0700

    first commit

---
+++ resources/views/dashboard.blade.php
@@ -0,0 +1,57 @@
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            {{ __('Dashboard') }}
+        </h2>
+    </x-slot>
+
+    <div class="py-12">
+        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+                <div class="flex">
+                    <div class="flex-auto text-2xl mb-4">Tasks List<svg class="w-5 inline-block ml-2"xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M2 11a1 1 0 011-1h2a1 1 0 011 1v5a1 1 0 01-1 1H3a1 1 0 01-1-1v-5zM8 7a1 1 0 011-1h2a1 1 0 011 1v9a1 1 0 01-1 1H9a1 1 0 01-1-1V7zM14 4a1 1 0 011-1h2a1 1 0 011 1v12a1 1 0 01-1 1h-2a1 1 0 01-1-1V4z" />
+                        </svg></div>
+
+                    <div class="flex-auto text-right mt-2">
+                        <a href="/task" class="btn-grad">Add new Task</a>
+                    </div>
+                </div>
+                <table class="w-full text-md rounded mb-4">
+                    <thead>
+                        <tr class="border-b">
+                            <th class="text-left p-3 px-5">Task</th>
+                            <th class="text-left p-3 px-5">Actions</th>
+                            <th></th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        @foreach(auth()->user()->tasks as $task)
+                        <tr class="border-b-g">
+                            <td class="p-3 px-5">
+                                {{$task->description}}
+                            </td>
+                            <td class="p-3 px-5 ">
+
+                                <a href="/task/{{$task->id}}" name="edit" class="mr-3 text-sm bg-blue-500 hover:bg-blue-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Edit<svg class="w-4 inline-block ml-2 " xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                        <path d="M17.414 2.586a2 2 0 00-2.828 0L7 10.172V13h2.828l7.586-7.586a2 2 0 000-2.828z" />
+                                        <path fill-rule="evenodd" d="M2 6a2 2 0 012-2h4a1 1 0 010 2H4v10h10v-4a1 1 0 112 0v4a2 2 0 01-2 2H4a2 2 0 01-2-2V6z" clip-rule="evenodd" />
+                                    </svg></a>
+                                <form action="/task/{{$task->id}}" class="inline-block">
+                                    <button type="submit" name="delete" formmethod="POST" class="text-sm bg-red-500 hover:bg-red-700 text-white py-1 px-2 rounded focus:outline-none focus:shadow-outline">Delete<svg class="w-4 inline-block ml-1 " xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor">
+                                            <path fill-rule="evenodd" d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0V8zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z" clip-rule="evenodd" />
+                                        </svg></button>
+                                    {{ csrf_field() }}
+                                </form>
+                            </td>
+                        </tr>
+
+
+                        @endforeach
+                    </tbody>
+                </table>
+                <img class="mx-auto object-cover h-80 w-96 " src="https://media.giphy.com/media/sIIhZliB2McAo/giphy.gif">
+            </div>
+        </div>
+    </div>
+</x-app-layout>
