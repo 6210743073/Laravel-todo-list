5821855a7cb1a5025a23139039182653d8467eb9
Eakkasitt <48272428+Zephy1337@users.noreply.github.com>
2022-03-23 09:48:32 +0700

    restore to default

--- tests/Feature/FunctionTest.php
+++ tests/Feature/FunctionTest.php
@@ -43,7 +43,7 @@ class FunctionTest extends TestCase
                  'description' => 'Test Task'
              ]);
 
-        $response->assertStatus(404);
+        $response->assertStatus(302);
         $response->assertRedirect(RouteServiceProvider::HOME);
     }
 
