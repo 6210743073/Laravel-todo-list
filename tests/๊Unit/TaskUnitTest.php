fa802e0c2b8458b5c59e43741d70abed7b1d73ff
6210743073 <poohmiphan@outlook.com>
2022-04-20 09:58:40 +0700

    test seeder

--- tests/๊Unit/TaskUnitTest.php
+++ tests/๊Unit/TaskUnitTest.php
@@ -2,9 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Models\User;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
-
+use Illuminate\Support\Str;
 class TaskUnitTest extends TestCase
 {
 
@@ -13,19 +14,51 @@ class TaskUnitTest extends TestCase
     public function test_schema_description()
     {
     }
+
+    #Validation task
     public function test_schema_useingOnly_Thai_description()
     {
-    }
-    public function test_schema_useingOnly_Eng_description()
-    {
+        $response = $this->actingAs($user = User::factory()->create())
+            ->post('/task', [
+                'description' => 'สวัสดีวันจันทร์'
+            ]);
+
+        $response->assertStatus(302);
     }
     public function test_schema_useingOnly_Num_description()
     {
+        $response = $this->actingAs($user = User::factory()->create())
+            ->post('/task', [
+                'description' => 'สวัสดีวันจันทร์'
+            ]);
+
+        $response->assertStatus(302);
     }
     public function test_schema_useingOnly_SQL_injection_description()
     {
+        $response = $this->actingAs($user = User::factory()->create())
+            ->post('/task', [
+                'description' => 'txtUserId = getRequestString("UserId");'
+            ]);
+
+        $response->assertStatus(302);
     }
     public function test_schema_useingOnly_Null_description()
     {
+        $response = $this->actingAs($user = User::factory()->create())
+            ->post('/task', [
+                'description' => null
+            ]);
+
+        $response->assertStatus(302);
+    }
+    public function test_schema_1000char_description()
+    {
+        $response = $this->actingAs($user = User::factory()->create())
+            ->post('/task', [
+                'description' => Str::random(1000),
+            ]);
+
+        $response->assertStatus(302);
     }
 }
